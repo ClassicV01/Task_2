2491f6914790428c729238e88ac233176abe8099
Igor Korostelev <denisclassicv@gmail.com>
2023-08-11 16:44:27 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Task_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(854, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem1});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem1
+            // 
+            this.открытьToolStripMenuItem1.Name = "открытьToolStripMenuItem1";
+            this.открытьToolStripMenuItem1.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem1.Text = "Открыть";
+            this.открытьToolStripMenuItem1.Click += new System.EventHandler(this.открытьToolStripMenuItem1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(122, 26);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(854, 406);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Excel|*.xls";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripComboBox1,
+            this.toolStripLabel2,
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(854, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(169, 22);
+            this.toolStripLabel1.Text = "Список загруженных файлов";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(300, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(142, 22);
+            this.toolStripLabel2.Text = "Отобразить данные с БД";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::Task_2.Properties.Resources.Button;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "Отобразить данные с БД";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 430);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelDataReader;
+using System.IO;
+using Microsoft.Data.SqlClient;
+
+namespace Task_2
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void открытьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var res = openFileDialog1.ShowDialog();
+                if (res == DialogResult.OK)
+                {
+                    fileName = openFileDialog1.FileNames;
+                    if(fileName.Length > 1)
+                    {
+                        foreach(var file in fileName)
+                        {                   
+                            toolStripComboBox1.Items.Add(file);
+                            await OpenAndImportExcelFile(file);
+                        }
+                    }
+                    else
+                    {
+                        Text = fileName[0];
+
+                        await OpenAndImportExcelFile(Text);
+                    }
+
+                }
+                else
+                {
+                    throw new Exception("Неверный файл!");
+                }
+                toolStripComboBox1.SelectedIndex = 0;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task OpenAndImportExcelFile(string path)
+        {
+            using(FileStream fs = File.OpenRead(path))
+            {
+                
+                IExcelDataReader reader = ExcelReaderFactory.CreateReader(fs);
+
+                DataSet ds = reader.AsDataSet(new ExcelDataSetConfiguration()
+                {
+                    ConfigureDataTable = (x) => new ExcelDataTableConfiguration()
+                    {
+                        UseHeaderRow = false
+                    }
+                });
+                
+                tableCollection = ds.Tables;
+
+                foreach (DataTable table in tableCollection)
+                {
+
+                    var arr = table.Select();
+                    using (SqlConnection connection = new SqlConnection(connectionStringToDB))
+                    {
+                        await connection.OpenAsync();
+                        try
+                        {
+                            SqlCommand commandDefault = new SqlCommand();
+                            commandDefault.Connection = connection;
+                            commandDefault.CommandText = @"INSERT INTO Class_2 VALUES (@CHECKS, @OPENING_BALANCE_ACTIVE, @OPENING_BALANCE_PASSIVE, @TURNS_DEBIT, @TURNS_CREDIT, @OUTGOING_BALANCE_ACTIVE, @OUTGOING_BALANCE_PASSIVE)";
+                            commandDefault.Parameters.Add("@CHECKS", SqlDbType.NVarChar);
+                            commandDefault.Parameters.Add("@OPENING_BALANCE_ACTIVE", SqlDbType.NVarChar);
+                            commandDefault.Parameters.Add("@OPENING_BALANCE_PASSIVE", SqlDbType.NVarChar);
+                            commandDefault.Parameters.Add("@TURNS_DEBIT", SqlDbType.NVarChar);
+                            commandDefault.Parameters.Add("@TURNS_CREDIT", SqlDbType.NVarChar);
+                            commandDefault.Parameters.Add("@OUTGOING_BALANCE_ACTIVE", SqlDbType.NVarChar);
+                            commandDefault.Parameters.Add("@OUTGOING_BALANCE_PASSIVE", SqlDbType.NVarChar);
+
+                            commandDefault.Parameters["@CHECKS"].Value = arr[8][0].ToString();
+                            commandDefault.Parameters["@OPENING_BALANCE_ACTIVE"].Value = DBNull.Value;
+                            commandDefault.Parameters["@OPENING_BALANCE_PASSIVE"].Value = DBNull.Value;
+                            commandDefault.Parameters["@TURNS_DEBIT"].Value = DBNull.Value;
+                            commandDefault.Parameters["@TURNS_CREDIT"].Value = DBNull.Value;
+                            commandDefault.Parameters["@OUTGOING_BALANCE_ACTIVE"].Value = DBNull.Value;
+                            commandDefault.Parameters["@OUTGOING_BALANCE_PASSIVE"].Value = DBNull.Value;
+
+                            await commandDefault.ExecuteNonQueryAsync();
+                        }
+                        catch (SqlException ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        for (int i = 9; i < arr.Length - 1; i++)
+                        {
+                            if (arr[i][0].ToString() != "ПО КЛАССУ")
+                            {
+                                try
+                                {
+                                    SqlCommand command = new SqlCommand();
+                                    command.Connection = connection;
+                                    command.CommandText = @"INSERT INTO Class_2 VALUES (@CHECKS, @OPENING_BALANCE_ACTIVE, @OPENING_BALANCE_PASSIVE, @TURNS_DEBIT, @TURNS_CREDIT, @OUTGOING_BALANCE_ACTIVE, @OUTGOING_BALANCE_PASSIVE)";
+                                    command.Parameters.Add("@CHECKS", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@OPENING_BALANCE_ACTIVE", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@OPENING_BALANCE_PASSIVE", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@TURNS_DEBIT", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@TURNS_CREDIT", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@OUTGOING_BALANCE_ACTIVE", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@OUTGOING_BALANCE_PASSIVE", SqlDbType.NVarChar);
+
+                                    command.Parameters["@CHECKS"].Value = arr[i][0].ToString();
+                                    command.Parameters["@OPENING_BALANCE_ACTIVE"].Value = arr[i][1];
+                                    command.Parameters["@OPENING_BALANCE_PASSIVE"].Value = arr[i][2];
+                                    command.Parameters["@TURNS_DEBIT"].Value = arr[i][3];
+                                    command.Parameters["@TURNS_CREDIT"].Value = arr[i][4];
+                                    command.Parameters["@OUTGOING_BALANCE_ACTIVE"].Value = arr[i][5];
+                                    command.Parameters["@OUTGOING_BALANCE_PASSIVE"].Value = arr[i][6];
+
+                                    await command.ExecuteNonQueryAsync();
+                                }
+                                catch (SqlException ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+
+                            }
+                            else
+                            {
+                                i++;
+                                if (i != arr.Length - 1)
+                                {
+                                    SqlCommand command = new SqlCommand();
+                                    command.Connection = connection;
+                                    command.CommandText = @"INSERT INTO Class_2 VALUES (@CHECKS, @OPENING_BALANCE_ACTIVE, @OPENING_BALANCE_PASSIVE, @TURNS_DEBIT, @TURNS_CREDIT, @OUTGOING_BALANCE_ACTIVE, @OUTGOING_BALANCE_PASSIVE)";
+                                    command.Parameters.Add("@CHECKS", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@OPENING_BALANCE_ACTIVE", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@OPENING_BALANCE_PASSIVE", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@TURNS_DEBIT", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@TURNS_CREDIT", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@OUTGOING_BALANCE_ACTIVE", SqlDbType.NVarChar);
+                                    command.Parameters.Add("@OUTGOING_BALANCE_PASSIVE", SqlDbType.NVarChar);
+
+                                    command.Parameters["@CHECKS"].Value = arr[i][0].ToString();
+                                    command.Parameters["@OPENING_BALANCE_ACTIVE"].Value = DBNull.Value;
+                                    command.Parameters["@OPENING_BALANCE_PASSIVE"].Value = DBNull.Value;
+                                    command.Parameters["@TURNS_DEBIT"].Value = DBNull.Value;
+                                    command.Parameters["@TURNS_CREDIT"].Value = DBNull.Value;
+                                    command.Parameters["@OUTGOING_BALANCE_ACTIVE"].Value = DBNull.Value;
+                                    command.Parameters["@OUTGOING_BALANCE_PASSIVE"].Value = DBNull.Value;
+
+                                    await command.ExecuteNonQueryAsync();
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private async void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionStringToDB))
+            {
+                await connection.OpenAsync();
+                string command = @"SELECT * FROM Class_2";
+                SqlDataAdapter adapter = new SqlDataAdapter(command, connection);
+
+                DataSet ds = new DataSet();
+                adapter.Fill(ds);
+                dataGridView1.DataSource = ds.Tables[0];
+
+            }
+        }
+
+        private string[] fileName = null;
+        private DataTableCollection tableCollection = null;
+        private string connectionStringToDB = "Server=DESKTOP-4HNKCF1;Database=Training OCB 1;Trusted_Connection=True;TrustServerCertificate=True;";
+
+    }
+}
